91934bec7ec551e850d675aa63b62cd8386ddd6a
Florian Hofmair <fhofmair@gmail.com>
2021-02-28 13:11:37 +0100

    Always initialize new graphs with required parameters

--- Overmine.Editor/Assets/Editor/Graph/GraphCallbacks.cs
+++ Overmine.Editor/Assets/Editor/Graph/GraphCallbacks.cs
@@ -1,4 +1,5 @@
 using Data;
+using Editor.Graph.Parameters;
 using GraphProcessor;
 using UnityEditor;
 using UnityEditor.Callbacks;
@@ -8,10 +9,22 @@ namespace Editor.Graph
 {
 	public static class GraphCallbacks
 	{
+		private static string[] _requiredParameters = {
+			"Self",
+			"Owner",
+			"Root",
+			"Link"
+		};
+		
 		[MenuItem("Assets/Create/Data/Behaviour Graph")]
 		public static void CreateGraph()
 		{
 			var graph = ScriptableObject.CreateInstance<BehaviourGraph>();
+			foreach (var param in _requiredParameters)
+			{
+				graph.AddExposedParameter(param, typeof(EntityParameter));
+			}
+			
 			ProjectWindowUtil.CreateAsset(graph, "Behaviour.asset");
 		}
 		
